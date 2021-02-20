963edd992e5cb5ea11f97eaf9d5fedac8f55253e
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-02-19 22:08:06 -0800

    update user model

--- server/model/User.ts
+++ server/model/User.ts
@@ -1,6 +1,27 @@
-import { Schema, Document, model, models } from "mongoose";
+import Adapters from "next-auth/adapters";
 
-interface User extends Document {
-	name: string;
-	role: "admin" | "voter";
+class User extends (<any>Adapters.TypeORM.Models.User.model) {
+	constructor(name, email, image, emailVerified, role) {
+		super(name, email, image, emailVerified);
+		this.role = "Client";
+	}
 }
+
+const UserSchema = {
+	name: "User",
+	target: User,
+	columns: {
+		...Adapters.TypeORM.Models.User.schema.columns,
+		role: {
+			type: "varchar",
+			nullable: true,
+		},
+	},
+};
+
+export default {
+	User: {
+		model: User,
+		schema: UserSchema,
+	},
+};

3a8c5093435837d787669c89b7e12dfb2ca06f1f
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-02-22 20:41:21 -0800

    add new font

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<link rel="preconnect" href="https://fonts.gstatic.com" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap"
+						rel="stylesheet"
+					/>
+					<link rel="icon" href="/svg/logo.webp" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;

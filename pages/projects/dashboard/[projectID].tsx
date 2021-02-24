f44277c500c0b80dff00ec202785c8d36bf217fa
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-02-24 15:53:10 -0800

    turn component into server component

--- pages/projects/dashboard/[projectID].tsx
+++ pages/projects/dashboard/[projectID].tsx
@@ -4,32 +4,63 @@ import { useRouter } from "next/router";
 import Navbar from "../../../components/navbar/Navbar";
 import Dashboard from "../../../components/dashboard/Dashboard";
 import styles from "../../../styles/Home.module.scss";
-const projectTask = () => {
-	const { query } = useRouter();
-	// const [planned, setPlanned] = React.useState([])
-	// const [planned, setPlanned] = React.useState([])
-	// const [planned, setPlanned] = React.useState([])
+import Loading from "../../../components/loading/Loading";
+import { Task } from "../../../types/Task";
+import { useQuery } from "react-query";
+import { fetcher } from "../../../utils/fetcher";
+import { distributeTask } from "../../../utils/distributeTask";
+import { GetServerSideProps } from "next";
 
-	const projectId = query.projectID;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const { query } = context;
+	return {
+		props: { query },
+	};
+};
 
-	const { data: response, isLoading } = useTasks(
-		`/api/projects/dashboard/${projectId}`
+interface AppProps {
+	query: { projectID: string };
+}
+
+const projectTask = (props: AppProps) => {
+	const { query } = props;
+
+	const projectID = query.projectID as string;
+	const url = `/api/projects/dashboard/${projectID}`;
+
+	const { data, isLoading, isError, error } = useQuery(
+		["tasks", projectID],
+		async () => {
+			const res = await fetcher(url);
+			return distributeTask(res.data.tasks);
+		}
 	);
 
-	console.log(response);
+	if (isLoading) return <Loading />;
+	if (isError) return <span>Error: {error.message}</span>;
 
 	return (
 		<div>
 			<Navbar />
 			<div className={styles.main__container}>
 				<div className={styles.dashboard__container}>
-					<Dashboard status="Planned" tasks={[]} color="#1fa0ff" />
-					<Dashboard
-						status="In Progress"
-						tasks={[]}
-						color="#c17aff"
-					/>
-					<Dashboard status="Complete" tasks={[]} color="#6dd345" />
+					<>
+						<Dashboard
+							status="Planned"
+							tasks={data?.planned}
+							color="#1fa0ff"
+						/>
+						<Dashboard
+							status="In Progress"
+							tasks={data?.inProgress}
+							color="#c17aff"
+						/>
+						<Dashboard
+							status="Complete"
+							tasks={data?.complete}
+							color="#6dd345"
+						/>
+					</>
 				</div>
 			</div>
 		</div>

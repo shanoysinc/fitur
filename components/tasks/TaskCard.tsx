20410ed732c2951f210be5b48eac9a0355de2070
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-03-01 23:19:58 -0800

    create taskcard component

---
+++ components/tasks/TaskCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../../styles/tasks/taskCard.module.scss";
+const TaskCard = () => {
+	return (
+		<div className={styles.container}>
+			<p>Learn to code </p>
+		</div>
+	);
+};
+
+export default TaskCard;

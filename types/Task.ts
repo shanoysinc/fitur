06b60b33d8908d18b141acb86dad883558be8e72
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-03-02 14:42:33 -0800

    add current task type

--- types/Task.ts
+++ types/Task.ts
@@ -1,9 +1,13 @@
 import { Document } from "mongoose";
 
-export interface Task extends Document {
+export interface Task {
 	title: string;
 	description: string;
 	createAt: Date;
 	_id: string;
 	projectCardID: string;
+}
+
+export interface CurrentTask extends Task {
+	projectCardName: string;
 }

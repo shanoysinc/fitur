efd428c88edba5aa7d9b734ec8fff33214c5cc4d
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-03-12 21:11:47 -0800

    add project mutation

--- hooks/project.ts
+++ hooks/project.ts
@@ -1,13 +1,32 @@
 import { fetcher } from "../utils/fetcher";
-import { useQuery, useMutation } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 import axios from "axios";
+import { toastNotification } from "../utils/toastNotification";
+
 const url = `/api/projects`;
 
-export const useProject = () => {
+export const useProjects = () => {
 	return useQuery("projects", () => fetcher(url));
 };
 
-export const useProjectMutation = (url: string) => {
-	// const queryClient = useQueryClient();
-	return useMutation((updateProject) => axios.patch(url, updateProject));
+export const useProjectMutation = (currentProjectID: string) => {
+	const queryClient = useQueryClient();
+	return useMutation(
+		(updateProject) =>
+			axios.patch(`/api/projects/${currentProjectID}`, updateProject),
+		{
+			onSuccess: () => {
+				queryClient.invalidateQueries("project");
+			},
+			onError: (res) => {
+				toastNotification(res.data.message, "error");
+			},
+		}
+	);
+};
+
+export const useProject = (projectID: string) => {
+	return useQuery(["projects", projectID], () =>
+		fetcher(`/api/projects/${projectID}`)
+	);
 };

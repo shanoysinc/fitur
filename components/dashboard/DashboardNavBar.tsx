492c421e8938c741eed4782b9332b32ff0890f96
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-03-12 21:13:13 -0800

    use project mutation

--- components/dashboard/DashboardNavBar.tsx
+++ components/dashboard/DashboardNavBar.tsx
@@ -4,11 +4,14 @@ import { Project } from "../../types/Project";
 import StarIcon from "../../assets/StarIcon";
 import { useMutation } from "react-query";
 import axios from "axios";
-import OptionIcon from "../../assets/OptionIcon";
+import OptionsIcon from "../../assets/OptionsIcon";
+import GearIcon from "../../assets/GearIcon";
 import ProjectOptions from "../dropdown/projects/ProjectOptions";
 import { useClickOutSide } from "../../hooks/clickOutSide";
 import { useProjectMutation } from "../../hooks/project";
 import { toastNotification } from "../../utils/toastNotification";
+import DashboardOptions from "../dropdown/dashboard/DashboardOptions";
+
 interface AppProps extends Project {
 	projectID: string;
 }
@@ -22,14 +25,12 @@ const DashboardNavBar = ({
 	const [projectName, setProjectName] = React.useState(name);
 	const [projectNameEditor, setProjectNameEdiotr] = React.useState(false);
 	const [isStarred, setIsStarred] = React.useState(startValue);
-	const projectMutation = useProjectMutation(`/api/projects/${projectID}`);
+	const [showMenu, setShowMenu] = React.useState(false);
+	const projectMutation = useProjectMutation(projectID);
 
-	const mutation = useMutation((newStarValue) =>
-		axios.patch(`/api/projects/starred/${projectID}`, newStarValue)
-	);
 	const starHandler = () => {
 		const newValue = !isStarred;
-		mutation.mutate({ startValue: newValue });
+		projectMutation.mutate({ isStarred: newValue });
 		setIsStarred(newValue);
 	};
 
@@ -52,6 +53,9 @@ const DashboardNavBar = ({
 	};
 
 	const projectNameRef = useClickOutSide(() => setProjectNameEdiotr(false));
+	const optionsRef = useClickOutSide(() => setShowOptions(false));
+	const menuRef = useClickOutSide(() => setShowMenu(false));
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.container__left}>
@@ -89,18 +93,35 @@ const DashboardNavBar = ({
 
 				<div className={styles.vertical__bar}></div>
 
-				<div
-					className={styles.item__container}
-					onClick={() => toggleOptions(projectID)}
-				>
-					<div className={styles.nav__items}>
-						<OptionIcon fill="white" height={17} width={17} />
+				<div className={styles.item__container} ref={optionsRef}>
+					<div
+						className={styles.nav__items}
+						onClick={() => toggleOptions(projectID)}
+					>
+						<GearIcon fill="white" height={17} width={17} />
 						<p>Options</p>
 					</div>
 					{showOptions === projectID && (
 						<ProjectOptions
 							projectID={projectID}
 							closeDropdown={setShowOptions}
+						/>
+					)}
+				</div>
+			</div>
+			<div className={styles.container__right}>
+				<div ref={menuRef} className={styles.item__container}>
+					<div
+						className={styles.nav__items}
+						onClick={() => setShowMenu(!showMenu)}
+					>
+						<OptionsIcon fill="white" height={17} width={17} />
+						<p>Show menu</p>
+					</div>
+					{showMenu && (
+						<DashboardOptions
+							projectID={projectID}
+							closeDropdown={setShowMenu}
 						/>
 					)}
 				</div>
